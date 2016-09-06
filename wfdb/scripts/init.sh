7189795264c3c41600ae76faba12d6b245e696b7
predmach <chuckpace@gmail.com>
2016-09-06 11:55:29 -0700

    Created Image of WFDB with preinstalled core WFDB, python 2.7.12, pip and requirements.txt

---
+++ wfdb/scripts/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash/
+uname -a
+#bash $PREDMACH_DIR/installPython.sh
+bash $PREDMACH_DIR/install-wfdb.sh
---
+++ wfdb/scripts/installPython.sh
@@ -0,0 +1,31 @@
+#!/bin/bash/
+apt-get install -y build-essential checkinstall
+apt-get install -y libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+apt-get install -y wget
+
+#cd /usr/src
+#wget https://www.python.org/ftp/python/2.7.12/Python-2.7.12.tgz
+#tar xzf Python-2.7.12.tgz
+
+#cd Python-2.7.12
+#./configure
+
+#make altinstall
+
+#python2.7 -V
+python --version
+#/usr/bin/python2.7 --version
+#/usr/local/bin/python2.7 --version
+#which python
+#echo "alias python='/usr/local/bin/python2.7'" >> ~/.bashrc
+#source ~/.bashrc
+#. ~/.bashrc
+#echo "Setting Python************************************"
+#python --version
+#which python
+#cat ~/.bashrc
+#apt-get install python-pip
+
+#wget https://bootstrap.pypa.io/get-pip.py
+
+#/usr/local/bin/python2.7 get-pip.py
